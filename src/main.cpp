02cf63afcf9a5b560f7d8e0bc72bfa7c097d67fe
choushen <jacob.mckenzie@icloud.com>
2024-02-21 06:03:31 +0000

    rect bounces up and down

--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,78 @@
 int main()
 {
     Color darkGreen = Color{20, 160, 133, 255};
+    Color red = Color{255, 0, 0, 255};
+    Color rayWhite = Color{245, 245, 245, 255};
 
     const int screenWidth = 800;
-    const int screenHeight = 600;
+    const int screenHeight = 450;
+
+    int direction = 10;
 
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
 
-    int circleX = screenWidth / 2;
-    int circleY = screenHeight / 2;
+    // Circle variables
+    int circleX = screenWidth / 4;
+    int circleY = screenHeight / 4;
     int radius = 50;
-    Color red = Color{255, 0, 0, 255};
     
+    // Circle edges
+    int circleRight = circleX + radius;
+    int circleLeft = circleX - radius;
+    int circleUp = circleY - radius;
+    int circleDown = circleY + radius;
+
+    // Rectangle variables
+    int rectX {400};
+    int rectY{0};
+    int rectWidth{50}, rectHeight{50};
+
+    // Rectangle edges
+
+
+
+
+
     // While this function returns false, the window should stay open
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(darkGreen);
+        ClearBackground(rayWhite);
+
         // Add code to make sure the circle doesn't go off screen
 
-        if (circleX < 750) {
-            if (IsKeyDown(KEY_RIGHT)) circleX+=2;
-        }
 
-        if (circleX > 50) {
-            if (IsKeyDown(KEY_LEFT)) circleX-=2;
-        }
-
-        if (circleY < 550) {
-            if (IsKeyDown(KEY_DOWN)) circleY+=2;
-        }
-
-        if (circleY > 50) {
-            if (IsKeyDown(KEY_UP)) circleY-=2;
-        }
-
-
+        // Draw the circle and rectangle
+        DrawCircle(circleX, circleY, radius, darkGreen);
+        DrawRectangle(rectX, rectY, rectWidth, rectHeight, red);
         
-        if(IsKeyDown(KEY_EQUAL)) radius++;
+        // Making the rectangle go up and down
+        rectY += direction;
 
-        if(IsKeyDown(KEY_MINUS)) radius--;
+        if(rectY > 450 || rectY < 0) {
+            direction = -direction;
+        }
 
-        DrawCircle(circleX, circleY, radius, red);
+
+        if (IsKeyDown(KEY_RIGHT) && circleX < 750) {
+            circleX+=2;
+        }
+
+        if (IsKeyDown(KEY_LEFT) && circleX > 50) {
+            circleX-=2;
+        }
+
+        if (IsKeyDown(KEY_DOWN) && circleY < 550) {
+            circleY+=2;
+        }
+
+        if (IsKeyDown(KEY_UP) && circleY > 50) {
+            circleY-=2;
+        }
+
+
+
 
         EndDrawing();
     }
